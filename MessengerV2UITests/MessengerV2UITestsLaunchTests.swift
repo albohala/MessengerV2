b9d70cd9398f1fb6a6e37ec7e18d5c16557d7810
Abdullah Albohalika <aalbohalika@gmail.com>
2021-11-02 04:28:46 +0300

    Initial commit

---
+++ MessengerV2UITests/MessengerV2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MessengerV2UITestsLaunchTests.swift
+//  MessengerV2UITests
+//
+//  Created by administrator on 02/11/2021.
+//
+
+import XCTest
+
+class MessengerV2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
