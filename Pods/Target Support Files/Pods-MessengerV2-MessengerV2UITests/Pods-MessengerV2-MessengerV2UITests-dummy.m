779b4f5cb5df487d532aa816fab72386d28ff0e4
Abdullah Albohalika <aalbohalika@gmail.com>
2021-11-03 16:24:06 +0300

    deleted pods

--- Pods/Target Support Files/Pods-MessengerV2-MessengerV2UITests/Pods-MessengerV2-MessengerV2UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MessengerV2_MessengerV2UITests : NSObject
-@end
-@implementation PodsDummy_Pods_MessengerV2_MessengerV2UITests
-@end
--- Pods/Target Support Files/Pods-MessengerV2/Pods-MessengerV2-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MessengerV2 : NSObject
-@end
-@implementation PodsDummy_Pods_MessengerV2
-@end
